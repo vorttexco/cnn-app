655ba3b0842dbc92e2a0f053e7364a93918af530
Bruno Orcajo <bruno.orcajo@vorttex.com.br>
2024-08-24 01:04:14 +0200

    win32

--- lib/features/profile/profile_view.dart
+++ lib/features/profile/profile_view.dart
@@ -14,43 +14,63 @@ class ProfileView extends ProfileViewModel {
           fontWeight: FontWeight.w700,
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const Divider(
+      body: Column(
+        children: [
+          const Divider(
+            indent: AppConstants.KPADDING_DEFAULT,
+            endIndent: AppConstants.KPADDING_DEFAULT,
+            color: Color(0xFFD6D6D6),
+            thickness: 1,
+          ),
+          // ListTile(
+          //   contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+          //   onTap: () {
+          //     OneSignal.InAppMessages.addTrigger('open-config', 'true');
+          //   },
+          //   title: const CustomText(
+          //     'Notificações',
+          //     fontSize: 18,
+          //     fontWeight: FontWeight.w800,
+          //   ),
+          //   subtitle:
+          //       const CustomText('Configure o recebimento de notificações'),
+          //   trailing: SvgPicture.asset('assets/icons/arrow_forward.svg'),
+          // ),
+          // const Divider(
+          //   indent: AppConstants.KPADDING_DEFAULT,
+          //   endIndent: AppConstants.KPADDING_DEFAULT,
+          //   color: Color(0xFFD6D6D6),
+          //   height: 1,
+          //   thickness: 1,
+          // ),
+          // const Spacer(),
+          ListView.separated(
+            shrinkWrap: true,
+            physics: const BouncingScrollPhysics(),
+            itemCount: listOfMenu.length,
+            itemBuilder: (BuildContext context, int index) {
+              final menu = listOfMenu[index];
+              return SizedBox(
+                height: 50,
+                child: ListTile(
+                    onTap: () => openLink(menu),
+                    dense: true,
+                    title: CustomText(menu.title ?? '',
+                        fontWeight: FontWeight.w400),
+                    trailing:
+                        SvgPicture.asset('assets/icons/arrow_forward.svg')),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(
               indent: AppConstants.KPADDING_DEFAULT,
               endIndent: AppConstants.KPADDING_DEFAULT,
               color: Color(0xFFD6D6D6),
               thickness: 1,
             ),
-            ListView.separated(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(
-                  parent: BouncingScrollPhysics()),
-              itemCount: listOfMenu.length,
-              itemBuilder: (BuildContext context, int index) {
-                final menu = listOfMenu[index];
-                return SizedBox(
-                  height: 50,
-                  child: ListTile(
-                      onTap: () => openLink(menu),
-                      dense: true,
-                      title: CustomText(menu.title ?? '',
-                          fontWeight: FontWeight.w400),
-                      trailing:
-                          SvgPicture.asset('assets/icons/arrow_forward.svg')),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(
-                indent: AppConstants.KPADDING_DEFAULT,
-                endIndent: AppConstants.KPADDING_DEFAULT,
-                color: Color(0xFFD6D6D6),
-                thickness: 1,
-              ),
-            )
-          ],
-        ),
+          ),
+          const SizedBox(height: 49),
+        ],
       ),
     );
   }
