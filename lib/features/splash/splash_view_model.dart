12687b9c4eccb24cc513ed6937dafa645d68b2e8
JuanFranceschi <133984321+JuanFranceschi@users.noreply.github.com>
2024-09-12 12:38:11 -0300

    Tela de Notificações sendo chamada no inicio da aplicação na primeira vez

--- lib/features/splash/splash_view_model.dart
+++ lib/features/splash/splash_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:cnn_brasil_app/features/notifications_settings/notifications_settings.dart';
 import 'package:flutter/material.dart';
 
 import '../../core/index.dart';
@@ -7,10 +8,24 @@ abstract class SplashViewModel extends State<Splash> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3)).then(
-      (value) {
-        NavigatorManager(context).replacement(TabNavigator.route);
-      },
-    );
+
+    _checkAndStart();
+  }
+
+  _checkAndStart() async {
+    var viewNotificationValue = await StorageManager()
+        .getBool(AppConstants.SHARED_PREFERENCES_VIEW_NOTIFICATIONS_SETTINGS);
+
+    await Future.delayed(const Duration(seconds: 3));
+
+    if (viewNotificationValue == null) {
+      await Navigator.pushNamed(context, NotificationsSettings.route);
+      await StorageManager().setBool(
+        AppConstants.SHARED_PREFERENCES_VIEW_NOTIFICATIONS_SETTINGS,
+        true,
+      );
+    }
+
+    NavigatorManager(context).replacement(TabNavigator.route);
   }
 }
