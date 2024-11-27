14181f062d9a41329323f33ef13ff9a5e6deba3d
JuanFranceschi <133984321+JuanFranceschi@users.noreply.github.com>
2024-11-27 20:33:27 -0300

    Aplicativo sendo reconhecido pelo Android Auto

--- android/app/src/main/kotlin/com/cnn/brasil/MainActivity.kt
+++ android/app/src/main/kotlin/com/cnn/brasil/MainActivity.kt
@@ -4,5 +4,10 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
