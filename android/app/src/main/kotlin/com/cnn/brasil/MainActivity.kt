03d3204204b703c0416d3099c4575ba643faa88c
LuisSouza01 <luis.f.souza@outlook.com>
2025-03-31 19:04:22 -0300

    feat: SDK MDM

--- android/app/src/main/kotlin/com/cnn/brasil/MainActivity.kt
+++ android/app/src/main/kotlin/com/cnn/brasil/MainActivity.kt
@@ -4,5 +4,29 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import br.com.hands.mdm.libs.android.core.MDMCore
+import br.com.hands.mdm.libs.android.bundle.MDMBundle
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+
+      MDMCore.setDebugMode(true);
+      
+      val appId = "jmDLWQt0JAVA1iqY6J8MXRAoDY6wWQW1Wph1G3xV/AgrmkEaAiix3FLEk80xUI89BOh9FxFLqvyzR2ITK5zpN8kBhl5eHOTTFbGr+FaZwFEt0TFYMXbfXQnUwWQubpdwFAyQUA4Xjwz0J/4IP2R7si9cm+IporvUVRSyK2brIsBCBCsiOrJPw/FBcv69wmDBe2OxD9sN/VdjUL8eEk8EpIxg8obeF5Hqy5/0qEKCIhlN/TF9HTT1ugAvUUIYTn4YzcmcsrKQML0SaEv90OjH0CxSD6KI46LUM87ijd5ZLYadlzwzeeZ6lB4N4HPHbsQSa2exZ6Jrp0tZsWuWFS6J4Q=="
+
+      MDMCore.start(applicationContext, appId, object : OnStartListener {
+          override fun onStart() {
+              MDMBundle.start(applicationContext, object : OnStartListener {
+                  override fun onStart() {
+                      // MDM inicializado
+                  }
+              })
+          }
+      })
+  }
+
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+      super.configureFlutterEngine(flutterEngine)
+  }
+}
