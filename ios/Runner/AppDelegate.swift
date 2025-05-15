c7cbcbed960eaa84aa48805bf9c2e8feebcaf476
LuisSouza01 <luis.f.souza@outlook.com>
2025-05-15 19:53:50 -0300

    feat: mdm integration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -6,11 +6,13 @@ import MDMNotification
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   private let CHANNEL = "mdm_notification_channel"
+  private var notificationCenter = UNUserNotificationCenter.current()
 
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+  ) -> Bool {   
+
     MDMCore.setDebugMode(true)
 
     let appId = "jmDLWQt0JAVA1iqY6J8MXRAoDY6wWQW1Wph1G3xV/AgrmkEaAiix3FLEk80xUI89BOh9FxFLqvyzR2ITK5zpN8kBhl5eHOTTFbGr+FaZwFEt0TFYMXbfXQnUwWQubpdwFAyQUA4Xjwz0J/4IP2R7si9cm+IporvUVRSyK2brIsBCBCsiOrJPw/FBcv69wmDBe2OxD9sN/VdjUL8eEk8EpIxg8obeF5Hqy5/0qEKCIhlN/TF9HTT1ugAvUUIYTn4YzcmcsrKQML0SaEv90OjH0CxSD6KI46LUM87ijd5ZLYadlzwzeeZ6lB4N4HPHbsQSa2exZ6Jrp0tZsWuWFS6J4Q=="
@@ -18,6 +20,18 @@ import MDMNotification
     MDMCore.start(withAppId: appId, kitModules: [
       MDMNotification.self()
     ])
+
+    self.notificationCenter = UNUserNotificationCenter.current()
+    self.notificationCenter.delegate = self
+    self.notificationCenter.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+        if granted {
+            OperationQueue.main.addOperation({
+                if !UIApplication.shared.isRegisteredForRemoteNotifications {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            })
+        }
+    }
 
     guard let controller = window?.rootViewController as? FlutterViewController else {
       fatalError("Root view controller is not a FlutterViewController")
@@ -59,4 +73,37 @@ import MDMNotification
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    MDMNotification.registerToken(deviceToken)
+  }
+
+  override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    MDMNotification.unregisterToken()
+  }
+}
+
+extension AppDelegate {
+    override func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        if MDMNotification.isMDMNotification(userInfo) {
+            MDMNotification.receive(userInfo)
+        } else {
+            // Process your notification here
+        }
+        completionHandler();
+    }
+    
+    override func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        if MDMNotification.isMDMNotification(userInfo) {
+            MDMNotification.processNotification(userInfo, completionBlock: completionHandler)
+        } else {
+            // Open your notification here
+        }
+    }
 }
