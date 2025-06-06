0ada4eff9fff3f671b831d737a71f8d810ae04a0
Luis Souza <luissouza@Luiss-Mac-mini.local>
2025-06-06 14:37:16 -0300

    feat: add notification

---
+++ ios/group.br.com.cnn-brasil/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  group.br.com.cnn-brasil
+//
+//  Created by Luis Souza on 06/06/25.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
